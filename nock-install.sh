d50e8a5b1f7b9d44bf3f4bd88617241b05b6e65e
blockdark <90238014+darrrrrrrk@users.noreply.github.com>
2025-05-22 17:19:39 +0800

    Update nock-install.sh

--- nock-install.sh
+++ nock-install.sh
@@ -348,9 +348,9 @@ function start_miner_node() {
   echo -e "[*] 正在启动 Miner 节点（使用端口 $LEADER_PORT 和 $FOLLOWER_PORT）..."
   # 检查 nockchain 是否支持 --leader-port 和 --follower-port
   if ./target/release/nockchain --help | grep -q -- "--leader-port"; then
-    NOCKCHAIN_CMD="RUST_LOG=trace ./target/release/nockchain --mining-pubkey \"$PUBLIC_KEY\" --mine --leader-port $LEADER_PORT --follower-port $FOLLOWER_PORT"
+    NOCKCHAIN_CMD="RUST_LOG=trace ./target/release/nockchain --mining-pubkey \"$PUBLIC_KEY\" --mine --leader-port $LEADER_PORT --follower-port $FOLLOWER_PORT  --peer /ip4/156.67.62.71/udp/3006/quic-v1 --peer /ip4/5.9.104.252/udp/3006/quic-v1 --peer /ip4/65.108.107.111/udp/3006/quic-v1 --peer /ip4/47.86.7.251/udp/3006/quic-v1"
   else
-    NOCKCHAIN_CMD="RUST_LOG=trace ./target/release/nockchain --mining-pubkey \"$PUBLIC_KEY\" --mine"
+    NOCKCHAIN_CMD="RUST_LOG=trace ./target/release/nockchain --mining-pubkey \"$PUBLIC_KEY\" --mine --peer /ip4/156.67.62.71/udp/3006/quic-v1 --peer /ip4/5.9.104.252/udp/3006/quic-v1 --peer /ip4/65.108.107.111/udp/3006/quic-v1 --peer /ip4/47.86.7.251/udp/3006/quic-v1"
   fi
 
   # 在 screen 会话中运行 nockchain 命令，输出同时显示在 screen 和 miner.log
